a0a79e61696360a515ab712db7c00549a9493b00
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-03 18:58:26 +0800

    Make background

---
+++ src/components/Background.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Colors } from '../constants/colors';
+
+export const WelcomePageBackground = styled.div`
+    background: linear-gradient(${Colors.purple}, ${Colors.black});
+    // background: ${Colors.black};
+    background-blend-mode: multiply;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
