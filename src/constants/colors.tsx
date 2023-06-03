df3d8763d62668bef0432a4341bf0d1d5500a18d
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-03 18:58:50 +0800

    Add color constants

---
+++ src/constants/colors.tsx
@@ -0,0 +1,14 @@
+export const Colors = {
+    black: '#070707',
+    black_50: '#07070750',
+    purple: '#3E2252',
+    pink: '#FF2273',
+    pink_75: '#FF227375',
+    yellow: '#FFBD59',
+    blue: '#506EF8',
+    blue_50: '#506EF850',
+    blue_75: '#506EF875',
+    white: '#F8F8F8',
+    white_10: '#F8F8F810',
+    white_75: '#F8F8F875',
+}
