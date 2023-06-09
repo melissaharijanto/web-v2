fa79b315f9c642a46a7803ccb1e3a48f515eb5b0
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-10 00:49:55 +0800

    Add link constants

---
+++ src/constants/links.tsx
@@ -0,0 +1,20 @@
+export const LINKEDIN_URL = 'https://linkedin.com/in/melissaharijanto';
+export const RESUME_URL = '';
+export const GITHUB_URL = 'https://github.com/melissaharijanto';
+export const EMAIL_URL = 'mailto:melissaharijanto18@gmail.com'
+
+export const toLinkedIn = () => {
+    window.open(LINKEDIN_URL);
+}
+
+export const toResume = () => {
+    window.open(RESUME_URL);
+}
+
+export const toGithub = () => {
+    window.open(GITHUB_URL);
+}
+
+export const emailMe = () => {
+    window.open(EMAIL_URL);
+}
