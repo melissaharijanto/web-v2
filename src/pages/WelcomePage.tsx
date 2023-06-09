37797f2831816699d5fdd0c30b14899747a8861c
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-10 00:50:14 +0800

    Add Welcome Page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,34 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import { Colors } from '../constants/colors';
 import { WelcomePageBackground } from '../components/Background';
+import { toLinkedIn, toResume } from '../constants/links';
 
 const WelcomeText = styled.span`
     font-family: 'Metropolis-Bold', 'sans-serif';
     color: ${Colors.yellow};
     font-size: 2.5em;
-`
+`;
 
 const Container = styled.div`
-    background: linear-gradient(${Colors.blue_50} 10%, ${Colors.white_10} 90%);
+    background: linear-gradient(${Colors.blue_50} 10%, transparent 90%);
     padding: 2em;
     border-radius: 20px;
-`
+`;
 
 const Text = styled.span`
     font-family: 'Metropolis-SemiBold', 'sans-serif';
     color: ${Colors.white};
     font-size: 1.5em;
-`
+`;
 
 const LineBreak = styled.div`
     padding: 0.5em;
-`
+`;
 
 const LeftAlignedText = styled.div`
     text-align: left;
     display: flex;
     flex-direction: column;
-`
+`;
 
 const WhiteHorizontalLine = styled.hr`
     width: 25vw;
@@ -40,10 +41,13 @@ const WhiteHorizontalLine = styled.hr`
     margin-left: 0;
     margin-top: 1em;
     margin-bottom: 1em;
-`
+`;
 
-const Button = styled.button<{backgroundColor?: string}>`
-    background: ${props => props.backgroundColor? `linear-gradient(${props.backgroundColor}, transparent)` : `linear-gradient(${Colors.pink_75}, transparent)`};
+const Button = styled.button<{ backgroundColor?: string }>`
+    background: ${(props) =>
+        props.backgroundColor
+            ? `linear-gradient(${props.backgroundColor}, transparent)`
+            : `linear-gradient(${Colors.pink_75}, transparent)`};
     color: ${Colors.white};
     cursor: pointer;
     font-family: 'Metropolis-SemiBold', 'sans-serif';
@@ -51,37 +55,17 @@ const Button = styled.button<{backgroundColor?: string}>`
     padding: 0.5em 0.875em 0.5em 0.875em;
     border-radius: 10px;
     font-size: 1.25em;
-    transition: background 0.5s ease-out;
 
     :hover {
         background: linear-gradient(${Colors.black_50}, transparent);
     }
-`
+`;
 
-// const Text = styled.span`
-//     position: relative;
-//     width: 24em;
-//     margin: 0 auto;
-//     border-right: 2px solid rgba(255,255,255,.75);
-//     font-size: 180%;
-//     text-align: center;
-//     white-space: nowrap;
-//     overflow: hidden;
-//     transform: translateY(-50%);  
-//     color: white;  
-//     animation: typewriter 4s steps(44) 1s 1 normal both,
-//              blinkTextCursor 500ms steps(44) infinite normal;
-
-//     @keyframes typewriter{
-//         from{width: 0;}
-//         to{width: 24em;}
-//     }
-//     @keyframes blinkTextCursor{
-//         from{border-right-color: rgba(255,255,255,.75);}
-//         to{border-right-color: transparent;}
-//     }
-// `
-
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 90%;
+`;
 
 const WelcomePage = () => {
     return (
@@ -89,17 +73,26 @@ const WelcomePage = () => {
             <Container>
                 <LeftAlignedText>
                     <WelcomeText>welcome.</WelcomeText>
-                    <LineBreak/>
+                    <LineBreak />
                     <Text>you are now viewing</Text>
                     <Text>melissa's portfolio website.</Text>
-                    <WhiteHorizontalLine/>
-                    <div>
-                    <Button>Linkedin</Button>
-                    </div>
+                    <WhiteHorizontalLine />
+                    <Grid>
+                        <div>
+                            <Button onClick={toLinkedIn}>linkedin</Button>
+                        </div>
+                        <div>
+                            <Button
+                                backgroundColor={Colors.yellow_75}
+                                onClick={toResume}>
+                                resume
+                            </Button>
+                        </div>
+                    </Grid>
                 </LeftAlignedText>
             </Container>
         </WelcomePageBackground>
-    )
-}
+    );
+};
 
 export default WelcomePage;
