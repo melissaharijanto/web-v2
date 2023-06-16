8d372dd41079b65a0f0704fca0d1eecde274cb6b
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 00:09:57 +0800

    Modify design + erase unused constants (WelcomePage)

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
 import styled from 'styled-components';
 import { Colors } from '../constants/colors';
 import { WelcomePageBackground } from '../components/Background';
-import { toLinkedIn, toResume } from '../constants/links';
-import { SemiBoldText } from '../components/TextComponents';
-import { SPACER } from '../constants/constants';
-
-const WelcomeText = styled.span`
-    font-family: 'Metropolis-Bold', 'sans-serif';
-    color: ${Colors.yellow};
-    font-size: 2.5em;
-`;
+import { BoldText, RegularText, SemiBoldText } from '../components/TextComponents';
+import { NORMAL_SPACE, SPACER } from '../constants/constants';
+import { redirect } from '../constants/utils';
 
 const Container = styled.div`
     background: linear-gradient(${Colors.blue_50} 10%, transparent 90%);
@@ -21,17 +14,6 @@ const Container = styled.div`
     align-items: center;
     flex-direction: column;
 
-`;
-
-const Text = styled.span`
-    font-family: 'Metropolis-SemiBold', 'sans-serif';
-    color: ${Colors.white};
-    font-size: 1.25em;
-    margin-bottom: 0.5em;
-`;
-
-const LineBreak = styled.div`
-    padding: 0.5em;
 `;
 
 const WhiteHorizontalLine = styled.hr`
@@ -45,32 +27,6 @@ const WhiteHorizontalLine = styled.hr`
     margin-bottom: 1em;
 `;
 
-const Button = styled.button<{ backgroundColor?: string }>`
-    background: ${(props) =>
-        props.backgroundColor
-            ? `linear-gradient(${props.backgroundColor}, transparent)`
-            : `linear-gradient(${Colors.pink_75}, transparent)`};
-    color: ${Colors.white};
-    cursor: pointer;
-    font-family: 'Metropolis-SemiBold', 'sans-serif';
-    border: 2px solid ${Colors.white};
-    padding: 0.5em 0.875em 0.5em 0.875em;
-    border-radius: 10px;
-    font-size: 1em;
-
-    :hover {
-        background: linear-gradient(${Colors.black_50}, transparent);
-    }
-`;
-
-const Grid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    justify-items: center;
-    align-items: center;
-    width: 80%;
-`;
-
 const WelcomePage = () => {
     return (
         <WelcomePageBackground>
@@ -78,13 +34,24 @@ const WelcomePage = () => {
                 <SemiBoldText marginBottom='0.5em' fontSize='1.25em'>
                     hello! i am
                 </SemiBoldText>
-                <WelcomeText>
+                <BoldText color={Colors.yellow} fontSize='2.5em' textAlign='center'>
                     melissa anastasia harijanto
-                </WelcomeText>
+                </BoldText>
                 <WhiteHorizontalLine/>
-                <SemiBoldText>
+                <SemiBoldText marginBottom='1em' textAlign='center'>
                     nus computer science undergraduate{SPACER}frontend & web dev enthusiast
                 </SemiBoldText>
+                <div>
+                    <RegularText italic>
+                        click&nbsp;
+                    </RegularText>
+                    <RegularText italic hover color={Colors.blue} hoverColor={Colors.blue_75} onClick={() => redirect("https://www.spotify.com")}>
+                        here
+                    </RegularText>
+                    <RegularText italic>
+                        {NORMAL_SPACE}for design inspiration.
+                    </RegularText>
+                </div>
             </Container>
         </WelcomePageBackground>
     );
