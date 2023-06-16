32be2a7783244e24c9142a31f6a00178074c0b90
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 02:44:28 +0800

    Rename page

---
+++ src/pages/IntroAndProjectsPage.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { IntroAndProjectsBackground } from '../components/Background';
+import Profile from '../components/Profile';
+
+export const ContentDiv = styled.div`
+    width: 85%;
+`;
+
+const IntroAndProjectsPage = () => {
+    return (
+        <IntroAndProjectsBackground>
+            <ContentDiv>
+                <Profile />
+            </ContentDiv>
+        </IntroAndProjectsBackground>
+    );
+};
+
+export default IntroAndProjectsPage;
