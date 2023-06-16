0c187faf6d9465f00fe1bb5f401643e91ae1fefe
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 00:07:11 +0800

    Remove unused constants

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,30 +1,9 @@
 import styled from "styled-components";
 import { IntroAndProjects } from "../components/Background";
-import { Colors } from "../constants/colors";
 import Profile from "../components/Profile";
-import { BoldText } from "../components/TextComponents";
 
 const ContentDiv = styled.div`
     width: 85%;
-`
-
-const ProfileContainer = styled.div<{marginLeft?: string ; marginRight?: string}>`
-    background: linear-gradient(${Colors.purple}, transparent);
-    border-radius: 20px;
-    padding: 2em 3em 2em 3em;
-    margin-top: 2em;
-    margin-bottom: 2em;
-    margin-left: ${props => props.marginLeft? props.marginLeft : "2em"};
-    margin-right: ${props => props.marginRight? props.marginRight : "2em"};
-    display: flex;
-    flex-direction: column;
-`
-
-const TextDiv = styled.div<{flexDirection?: string}>`
-    padding-top: 1em;
-    padding-bottom: 1em;
-    display: flex;
-    flex-direction: ${props => props.flexDirection? props.flexDirection : "column"};
 `
 
 const ContentPage = () => {
