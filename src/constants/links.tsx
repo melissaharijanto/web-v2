fec720479201915a92988f281cf46dc8241673af
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 02:44:58 +0800

    Add behance link

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -2,6 +2,7 @@ export const LINKEDIN_URL = 'https://linkedin.com/in/melissaharijanto';
 export const RESUME_URL = '';
 export const GITHUB_URL = 'https://github.com/melissaharijanto';
 export const EMAIL_URL = 'mailto:melissaharijanto18@gmail.com';
+export const BEHANCE_URL = 'https://www.behance.net/melisara';
 
 export const toLinkedIn = () => {
     window.open(LINKEDIN_URL);
@@ -17,4 +18,8 @@ export const toGithub = () => {
 
 export const emailMe = () => {
     window.open(EMAIL_URL);
+};
+
+export const toBehance = () => {
+    window.open(BEHANCE_URL);
 };
