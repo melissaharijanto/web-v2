bc4cb511b10448b019b2e40b2df1c0913e0664be
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 03:41:28 +0800

    Add bullet point constant

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -2,3 +2,4 @@ export const EN_SPACER = '\u2002';
 export const SPACER = '\u2002'.concat('|').concat('\u2002');
 export const NORMAL_SPACE = <span>&nbsp;</span>;
 export const EM_DASH = '\u2014';
+export const BULLET_POINT = '\u2022';
