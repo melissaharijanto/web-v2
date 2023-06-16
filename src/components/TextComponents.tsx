3350ccac57a6c82e1fc7618ae09ef4e41240c052
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-17 00:06:56 +0800

    Add dynamic text components

--- src/components/TextComponents.tsx
+++ src/components/TextComponents.tsx
@@ -1,6 +1,44 @@
 import styled from 'styled-components';
 import { Colors } from '../constants/colors';
 
+
+export const RegularText= styled.span<{
+    color?: string;
+    italic?: boolean;
+    fontSize?: string;
+    marginBottom?: string;
+    hover?: boolean;
+    hoverColor?: string;
+    lineHeight?: string;
+    textAlign?: string;
+}>`
+    font-family: ${(props) =>
+        props.italic
+            ? `'Metropolis-RegularItalic', 'sans-serif'`
+            : `'Metropolis-Regular', 'sans-serif'`};
+    color: ${(props) => (props.color ? props.color : Colors.white)};
+    font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
+    margin-bottom: ${(props) =>
+        props.marginBottom ? props.marginBottom : '0'};
+    line-height: ${props => props.lineHeight ? props.lineHeight : "normal"} !important;
+    text-align: ${props => props.textAlign ? props.textAlign : "left"};
+
+    :hover {
+        color: ${(props) =>
+            props.hover
+                ? props.hoverColor
+                    ? props.hoverColor
+                    : props.color
+                    ? props.color
+                    : Colors.white
+                : props.color
+                ? props.color
+                : Colors.white};
+        cursor: ${(props) => (props.hover ? 'pointer' : 'default')};
+        text-decoration: ${(props) => (props.hover ? 'underline' : 'none')};
+    }
+`;
+
 export const SemiBoldText = styled.span<{
     color?: string;
     italic?: boolean;
@@ -20,7 +58,7 @@ export const SemiBoldText = styled.span<{
     margin-bottom: ${(props) =>
         props.marginBottom ? props.marginBottom : '0'};
     line-height: ${props => props.lineHeight ? props.lineHeight : "normal"} !important;
-    text-align: ${props => props.textAlign ? props.textAlign : "left"}
+    text-align: ${props => props.textAlign ? props.textAlign : "left"};
 
     :hover {
         color: ${(props) =>
@@ -42,10 +80,12 @@ export const BoldText = styled.span<{
     color?: string;
     fontSize?: string;
     marginBottom?: string;
+    textAlign?: string;
 }>`
     font-family: 'Metropolis-Bold', 'sans-serif';
     color: ${(props) => (props.color ? props.color : Colors.white)};
     font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
     margin-bottom: ${(props) =>
         props.marginBottom ? props.marginBottom : '0'};
+    text-align: ${props => props.textAlign ? props.textAlign : "left"};
 `;
