1ad93ca9fbf7b399a28190086b6d1cfdd1019b9a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-18 02:06:08 +0800

    Add Light Text

--- src/components/TextComponents.tsx
+++ src/components/TextComponents.tsx
@@ -1,11 +1,52 @@
 import styled from 'styled-components';
 import { Colors } from '../constants/colors';
 
+export const LightText = styled.span<{
+    color?: string;
+    italic?: boolean;
+    fontSize?: string;
+    marginBottom?: string;
+    marginTop?: string;
+    hover?: boolean;
+    hoverColor?: string;
+    lineHeight?: string;
+    textAlign?: string;
+}>`
+    font-family: ${(props) =>
+        props.italic
+            ? `'Metropolis-LightItalic', 'sans-serif'`
+            : `'Metropolis-Light', 'sans-serif'`};
+    color: ${(props) => (props.color ? props.color : Colors.white)};
+    font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
+    margin-bottom: ${(props) =>
+        props.marginBottom ? props.marginBottom : '0'};
+    margin-top: ${(props) => (props.marginTop ? props.marginTop : '0')};
+    line-height: ${(props) =>
+        props.lineHeight ? props.lineHeight : 'normal'} !important;
+    text-align: ${(props) => (props.textAlign ? props.textAlign : 'left')};
+
+    :hover {
+        color: ${(props) =>
+            props.hover
+                ? props.hoverColor
+                    ? props.hoverColor
+                    : props.color
+                    ? props.color
+                    : Colors.white
+                : props.color
+                ? props.color
+                : Colors.white};
+        cursor: ${(props) => (props.hover ? 'pointer' : 'default')};
+        text-decoration: ${(props) => (props.hover ? 'underline' : 'none')};
+    }
+`;
+
 export const RegularText = styled.span<{
     color?: string;
     italic?: boolean;
     fontSize?: string;
     marginBottom?: string;
+    marginTop?: string;
     hover?: boolean;
     hoverColor?: string;
     lineHeight?: string;
@@ -19,6 +60,7 @@ export const RegularText = styled.span<{
     font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
     margin-bottom: ${(props) =>
         props.marginBottom ? props.marginBottom : '0'};
+    margin-top: ${(props) => (props.marginTop ? props.marginTop : '0')};
     line-height: ${(props) =>
         props.lineHeight ? props.lineHeight : 'normal'} !important;
     text-align: ${(props) => (props.textAlign ? props.textAlign : 'left')};
@@ -44,6 +86,7 @@ export const SemiBoldText = styled.span<{
     italic?: boolean;
     fontSize?: string;
     marginBottom?: string;
+    marginTop?: string;
     hover?: boolean;
     hoverColor?: string;
     lineHeight?: string;
@@ -57,6 +100,7 @@ export const SemiBoldText = styled.span<{
     font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
     margin-bottom: ${(props) =>
         props.marginBottom ? props.marginBottom : '0'};
+    margin-top: ${(props) => (props.marginTop ? props.marginTop : '0')};
     line-height: ${(props) =>
         props.lineHeight ? props.lineHeight : 'normal'} !important;
     text-align: ${(props) => (props.textAlign ? props.textAlign : 'left')};
@@ -84,6 +128,7 @@ export const BoldText = styled.span<{
     textAlign?: string;
     italic?: boolean;
     lineHeight?: string;
+    marginTop?: string;
 }>`
     font-family: ${(props) =>
         props.italic
@@ -93,6 +138,7 @@ export const BoldText = styled.span<{
     font-size: ${(props) => (props.fontSize ? props.fontSize : '1em')};
     margin-bottom: ${(props) =>
         props.marginBottom ? props.marginBottom : '0'};
+    margin-top: ${(props) => (props.marginTop ? props.marginTop : '0')};
     text-align: ${(props) => (props.textAlign ? props.textAlign : 'left')};
     line-height: ${(props) =>
         props.lineHeight ? props.lineHeight : 'normal'} !important;
