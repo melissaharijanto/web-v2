c6713ae6463d7cb9ff4a3aaefd8eaa7fc1dfe0b0
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-18 02:06:59 +0800

    Add copyright symbol constant

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -3,3 +3,4 @@ export const SPACER = '\u2002'.concat('|').concat('\u2002');
 export const NORMAL_SPACE = <span>&nbsp;</span>;
 export const EM_DASH = '\u2014';
 export const BULLET_POINT = '\u2022';
+export const COPYRIGHT_SYMBOL = '\u00A9';
