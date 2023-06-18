b43af108069e174a4153585c20314a63ede24692
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-18 15:28:34 +0800

    Add new resume link

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,5 +1,6 @@
 export const LINKEDIN_URL = 'https://linkedin.com/in/melissaharijanto';
-export const RESUME_URL = 'https://drive.google.com/file/d/1L8POkttcbFa-k3ISztDwkUAnVZtyVIe6/view?usp=sharing';
+export const RESUME_URL =
+    'https://drive.google.com/file/d/11TjWo-55mOeTio7dKnydA2qOFplJOjAx/view?usp=sharing';
 export const GITHUB_URL = 'https://github.com/melissaharijanto';
 export const EMAIL_URL = 'mailto:melissaharijanto18@gmail.com';
 export const BEHANCE_URL = 'https://www.behance.net/melisara';
