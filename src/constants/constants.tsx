225e8b431d952ca31c0299088ea9e65424c3dcaf
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-08-17 23:23:46 +0800

    Add screen sizes

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -4,3 +4,10 @@ export const NORMAL_SPACE = <span>&nbsp;</span>;
 export const EM_DASH = '\u2014';
 export const BULLET_POINT = '\u2022';
 export const COPYRIGHT_SYMBOL = '\u00A9';
+
+// screen sizes
+export const sm = '@media (min-width: 640px)';
+export const md = '@media (min-width: 768px)';
+export const lg = '@media (min-width: 1028px)';
+export const xl = '@media (min-width: 1280px)';
+export const two_xl = '@media (min-width: 1536px)';
