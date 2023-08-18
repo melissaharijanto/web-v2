9ba1fd52f39ba35e817cb2de793369b054137e7f
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-08-18 15:35:07 +0800

    Make experience page responsive

--- src/components/experience-components/ExperienceHeader.tsx
+++ src/components/experience-components/ExperienceHeader.tsx
@@ -2,21 +2,38 @@ import styled from 'styled-components';
 import { Colors } from '../../constants/colors';
 import Experience from '../../resources/images/experience-icon.svg';
 import { BoldText, SemiBoldText } from '../TextComponents';
+import { screens } from '../../constants/constants';
 
 const HeaderDiv = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ${screens.md} {
+        flex-direction: row;
+    }
 `;
 
 const ExperienceDiv = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
-    margin-left: 3em;
+    margin-left: 0;
+    align-items: center;
+    justify-content: center;
+
+    ${screens.md} {
+        align-items: flex-start;
+        margin-left: 3em;
+    }
 `;
 
 const ExperienceIcon = styled.img`
-    width: 10vw;
-    // color: ${Colors.blue};
+    width: 30vw;
+    margin-bottom: 1em;
+    ${screens.md} {
+        width: 10vw;
+        margin-bottom: 0;
+    }
 `;
 
 const ExperienceHeader = () => {
@@ -24,12 +41,13 @@ const ExperienceHeader = () => {
         <HeaderDiv>
             <ExperienceIcon src={Experience} />
             <ExperienceDiv>
-                <BoldText fontSize="3.5em">experience</BoldText>
+                <BoldText fontSize="3.5em" mobileFontSize='1.75em'>experience</BoldText>
                 <SemiBoldText
-                    textAlign="left"
+                    textAlign="center"
                     marginTop="0.75em"
                     color={Colors.yellow}
-                    fontSize="1.25em">
+                    fontSize="1.25em"
+                    mobileFontSize='1em'>
                     a list of my relevant experiences & activities
                 </SemiBoldText>
             </ExperienceDiv>
